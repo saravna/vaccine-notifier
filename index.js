d20fd4d9b52d019d2e55ff7ddb23cd53cc65b099
saravana <saravana@codingmart.com>
2021-06-21 11:20:01 +0530

    error handling

--- index.js
+++ index.js
@@ -19,52 +19,57 @@ async function fetchAvailability () {
   for (let i = 0; i < toList.length ; i+=1) {
     let availableSessions = [];
     const district = process.env[`district${i}`];
-    const res = await fetch(`${url}?district_id=${district}&date=${dateInFormat}`)
-    const data = await res.json();
-    data.centers.forEach(center => {
-      center.sessions.forEach(session => {
-        if (
-          (session.available_capacity_dose1 > 0) 
-          && (district  === '539' ? session.min_age_limit === 18 : session.min_age_limit >=18 )
-          && session.vaccine === 'COVISHIELD') {
-          availableSessions = [ ...availableSessions, {
-            name: center.name,
-            date: session.date,
-            age: session.min_age_limit,
-            dose1: session.available_capacity_dose1,
-            // dose2: session.available_capacity_dose2,
-            vaccine: session.vaccine,
-          } ]
-        }
-      })
-    });
-    console.log(availableSessions.length);
-    if (availableSessions.length > 0) {
-      let html = `
-        <table style="border-collapse: collapse;">
-          <tr style="border-collapse: collapse;">
-            <th style="border: 1px solid black; border-collapse: collapse;">Name</th>
-            <th style="border: 1px solid black; border-collapse: collapse;">Date</th>
-            <th style="border: 1px solid black; border-collapse: collapse;">Vaccine</th>
-            <th style="border: 1px solid black; border-collapse: collapse;">Min Age</th>
-            <th style="border: 1px solid black; border-collapse: collapse;">Dose 1</th>
-            </tr>
-      `;
-            // <th>Dose 2</th>
-      availableSessions.forEach(session => {
-        html += `
-          <tr>
-            <td style="border: 1px solid black; border-collapse: collapse;">${session.name}</td>
-            <td style="border: 1px solid black; border-collapse: collapse;">${session.date}</td>
-            <td style="border: 1px solid black; border-collapse: collapse;">${session.vaccine}</td>
-            <td style="border: 1px solid black; border-collapse: collapse;">${session.age}</td>
-            <td style="border: 1px solid black; border-collapse: collapse;">${session.dose1}</td>
-            </tr>
-            `
-            // <td>${session.dose2}</td>
-      })
-      html += '</table>'
-      await sendMail(html, process.env[`to${i}`]);
+    try {
+      const res = await fetch(`${url}?district_id=${district}&date=${dateInFormat}`)
+      const data = await res.json();
+      data.centers.forEach(center => {
+        center.sessions.forEach(session => {
+          if (
+            (session.available_capacity_dose1 > 0) 
+            && (district  === '539' ? session.min_age_limit === 18 : session.min_age_limit >=18 )
+            && session.vaccine === 'COVISHIELD') {
+            availableSessions = [ ...availableSessions, {
+              name: center.name,
+              date: session.date,
+              age: session.min_age_limit,
+              dose1: session.available_capacity_dose1,
+              // dose2: session.available_capacity_dose2,
+              vaccine: session.vaccine,
+            } ]
+          }
+        })
+      });
+      console.log(availableSessions.length);
+      if (availableSessions.length > 0) {
+        let html = `
+          <table style="border-collapse: collapse;">
+            <tr style="border-collapse: collapse;">
+              <th style="border: 1px solid black; border-collapse: collapse;">Name</th>
+              <th style="border: 1px solid black; border-collapse: collapse;">Date</th>
+              <th style="border: 1px solid black; border-collapse: collapse;">Vaccine</th>
+              <th style="border: 1px solid black; border-collapse: collapse;">Min Age</th>
+              <th style="border: 1px solid black; border-collapse: collapse;">Dose 1</th>
+              </tr>
+        `;
+              // <th>Dose 2</th>
+        availableSessions.forEach(session => {
+          html += `
+            <tr>
+              <td style="border: 1px solid black; border-collapse: collapse;">${session.name}</td>
+              <td style="border: 1px solid black; border-collapse: collapse;">${session.date}</td>
+              <td style="border: 1px solid black; border-collapse: collapse;">${session.vaccine}</td>
+              <td style="border: 1px solid black; border-collapse: collapse;">${session.age}</td>
+              <td style="border: 1px solid black; border-collapse: collapse;">${session.dose1}</td>
+              </tr>
+              `
+              // <td>${session.dose2}</td>
+        })
+        html += '</table>'
+        await sendMail(html, process.env[`to${i}`]);
+      }
+    }
+    catch (e) {
+      console.error('Error: ', e);
     }
   }
 }
